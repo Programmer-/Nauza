c9fb0a44984567ffc3f0f3f56d6a93813c133df8
Justus Njuru Mbugua <justnjuru@gmail.com>
2022-10-25 01:18:56 +0300

    updates

--- App.js
+++ App.js
@@ -1,16 +1,11 @@
 import React from "react";
-import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
-import WelcomeScreen from "./app/screens/WelcomeScreen";
-import ViewImageScreen from "./app/screens/ViewImageScreen";
-import AppButton from "./app/components/AppButton";
-
-import Card from "./app/components/Card";
-import ListingDetailsScreen from "./app/screens/ListingDetailsScreen";
-import MessagesScreen from "./app/screens/MessagesScreen";
+import Icon from "./app/components/Icon";
+import Screens from "./app/components/Screens";
+import { MaterialCommunityIcons } from "@expo/vector-icons";
 import ListItem from "./app/components/ListItem";
-import LIstItemSeparator from "./app/components/LIstItemSeparator";
+import AccountScreen from "./app/screens/AccountScreen";
+import ListingsScreen from "./app/screens/ListingsScreen";
 
 export default function App() {
-  return <MessagesScreen />;
+  return <ListingsScreen />;
 }
