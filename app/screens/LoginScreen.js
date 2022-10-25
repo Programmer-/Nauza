dfde423e0e6ee378d91ada0da7e289123337ca21
Justus Njuru Mbugua <justnjuru@gmail.com>
2022-10-26 00:31:29 +0300

    Login screen with validations

---
+++ app/screens/LoginScreen.js
@@ -0,0 +1,67 @@
+import React from "react";
+import { Image, StyleSheet } from "react-native";
+import AppButton from "../components/AppButton";
+import AppTextInput from "../components/AppTextInput";
+import Screens from "../components/Screens";
+import { Formik } from "formik";
+import * as Yup from "yup";
+import AppText from "../components/AppText";
+import ErrorMessage from "../components/ErrorMessage";
+import AppFormField from "../components/AppFormField";
+
+const validationSchema = Yup.object().shape({
+  email: Yup.string().required().email().label("Email"),
+  password: Yup.string().required().min(4).label("Password"),
+});
+
+function LoginScreen(props) {
+  return (
+    <Screens style={styles.container}>
+      <Image style={styles.logo} source={require("../assets/dalle.png")} />
+      <Formik
+        initialValues={{ email: "", password: "" }}
+        onSubmit={(values) => console.log(values)}
+        validationSchema={validationSchema}
+      >
+        {({ handleChange, handleSubmit, errors, setFieldTouched, touched }) => (
+          <>
+            <AppFormField
+              autoCapitalize="none"
+              autoCorrect={false}
+              icon="email"
+              keyboardType="email-address"
+              name="email"
+              placeholder="Email"
+              textContentType="emailAddress"
+            />
+            <AppFormField
+              autoCapitalize="none"
+              autoCorrect={false}
+              icon="lock"
+              name="password"
+              placeholder="Password"
+              secureTextEntry
+              textContentType="password"
+            />
+            <AppButton color="secondary" title="Login" onPress={handleSubmit} />
+          </>
+        )}
+      </Formik>
+    </Screens>
+  );
+}
+
+const styles = StyleSheet.create({
+  logo: {
+    width: 80,
+    height: 80,
+    alignSelf: "center",
+    marginTop: 50,
+    marginBottom: 20,
+  },
+  container: {
+    padding: 10,
+  },
+});
+
+export default LoginScreen;
