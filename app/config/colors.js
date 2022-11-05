585ab30b4c18d0f2bb3fdaba170aaaf867a1e263
Justus Njuru Mbugua <justnjuru@gmail.com>
2022-11-05 21:21:33 +0300

    new color palletes

--- app/config/colors.js
+++ app/config/colors.js
@@ -13,4 +13,12 @@ export default {
   fourth: "#7f838a",
   fifth: "#7e8c88",
   danger: "#ff5252",
+
+  sample1: "#5A5C5C",
+  sample2: "#040607",
+  sample3: "#32758A",
+  sample4: "#4AD4E9",
+  sample41: "#4ad5ea",
+  sample5: "#DFE8E5",
+  sample6: "#b6e0e4",
 };
