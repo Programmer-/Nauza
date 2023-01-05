8d72ac559f95b4acd2f5e9cf130d31a13aa9e6e3
Justus Njuru Mbugua <justnjuru@gmail.com>
2023-01-05 15:19:56 +0300

    created and added authNavigator with theme mods

--- App.js
+++ App.js
@@ -2,56 +2,19 @@ import React from "react";
 import { Button, Text } from "react-native";
 import Screen from "./app/components/Screen";
 import { createStackNavigator } from "@react-navigation/stack";
-import { NavigationContainer, useNavigation } from "@react-navigation/native";
+import { MaterialCommunityIcons } from "@expo/vector-icons";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { NavigationContainer, useNavigation } from "@react-navigation/native";
+import WelcomeScreen from "./app/screens/WelcomeScreen";
 import RegisterScreen from "./app/screens/RegisterScreen";
 import LoginScreen from "./app/screens/LoginScreen";
-import WelcomeScreen from "./app/screens/WelcomeScreen";
 import MessagesScreen from "./app/screens/MessagesScreen";
 import AccountScreen from "./app/screens/AccountScreen";
 import ListingDetailsScreen from "./app/screens/ListingDetailsScreen";
 import ListingEditScreen from "./app/screens/ListingEditScreen";
 import ViewImageScreen from "./app/screens/ViewImageScreen";
-import { MaterialCommunityIcons } from "@expo/vector-icons";
-
-const Link = () => {
-  const navigation = useNavigation();
-  return (
-    <Button
-      title="Click"
-      onPress={() => navigation.navigate("TweetDetails", { id: 1 })}
-    />
-  );
-};
-
-const Tweets = ({ navigation }) => (
-  <Screen>
-    <Text>Tweets</Text>
-    <Link />
-  </Screen>
-);
-const TweetDetails = ({ route }) => (
-  <Screen>
-    <Text>Tweet Details{route.params.id}</Text>
-  </Screen>
-);
-
-const Stack = createStackNavigator();
-const StackNavigator = () => (
-  <Stack.Navigator
-    screenOptions={{
-      headerStyle: { backgroundColor: "dodgerblue" },
-      headerTintColor: "white",
-    }}
-  >
-    <Stack.Screen name="Tweets" component={Tweets} />
-    <Stack.Screen
-      name="TweetDetails"
-      component={TweetDetails}
-      options={({ route }) => ({ title: route.params.id })}
-    />
-  </Stack.Navigator>
-);
+import AuthNavigator from "./app/navigators/AuthNavigator";
+import navigationTheme from "./app/navigators/navigationTheme";
 
 const Account = () => (
   <Screen>
@@ -71,7 +34,7 @@ const TabNavigator = () => (
   >
     <Tab.Screen
       name="Feed"
-      component={Tweets}
+      component={StackNavigator}
       options={{
         tabBarIcon: ({ size }) => (
           <MaterialCommunityIcons name="home" size={size} />
@@ -84,8 +47,8 @@ const TabNavigator = () => (
 
 export default function App() {
   return (
-    <NavigationContainer>
-      <TabNavigator />
+    <NavigationContainer theme={navigationTheme}>
+      <AuthNavigator />
     </NavigationContainer>
   );
 }
