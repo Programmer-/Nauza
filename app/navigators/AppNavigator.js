a418b3c561b09d6dd9f624199c47956654ba7075
Justus Njuru Mbugua <justnjuru@gmail.com>
2023-01-06 23:24:46 +0300

    refactoring and added account Navigator

--- app/navigators/AppNavigator.js
+++ app/navigators/AppNavigator.js
@@ -1,10 +1,8 @@
 import React from "react";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
-import AccountScreen from "../screens/AccountScreen";
-import MessagesScreen from "../screens/MessagesScreen";
 import ListingEditScreen from "../screens/ListingEditScreen";
-import { MaterialCommunityIcons } from "@expo/vector-icons";
 import FeedNavigator from "./FeedNavigator";
+import AccountNavigator from "./AccountNavigator";
 
 const Tab = createBottomTabNavigator();
 
@@ -12,7 +10,7 @@ const AppNavigator = () => (
   <Tab.Navigator>
     <Tab.Screen name="Feed" component={FeedNavigator} />
     <Tab.Screen name="ListingEdit" component={ListingEditScreen} />
-    <Tab.Screen name="Account" component={AccountScreen} />
+    <Tab.Screen name="Account" component={AccountNavigator} />
   </Tab.Navigator>
 );
 
