d6363d3300dd0239cb4bab2556a8e229b6049da6
Justus Njuru Mbugua <justnjuru@gmail.com>
2023-01-18 11:32:14 +0300

    Few changes

--- app/navigators/AppNavigator.js
+++ app/navigators/AppNavigator.js
@@ -3,15 +3,51 @@ import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import ListingEditScreen from "../screens/ListingEditScreen";
 import FeedNavigator from "./FeedNavigator";
 import AccountNavigator from "./AccountNavigator";
+import { MaterialCommunityIcons } from "@expo/vector-icons";
+import NewListingButton from "./NewListingButton";
 
 const Tab = createBottomTabNavigator();
 
 const AppNavigator = () => (
   <Tab.Navigator>
-    <Tab.Screen name="Feed" component={FeedNavigator} />
-    <Tab.Screen name="ListingEdit" component={ListingEditScreen} />
-    <Tab.Screen name="Account" component={AccountNavigator} />
+    <Tab.Screen
+      name="Feed"
+      component={FeedNavigator}
+      options={{
+        tabBarIcon: ({ color, size }) => (
+          <MaterialCommunityIcons name="home" color={color} size={size} />
+        ),
+      }}
+    />
+    {/* To Do Something With Icon and TouchableOpacity button here BEAUTIFYING THE TAB */}
+    <Tab.Screen
+      name="Add a Listing"
+      component={ListingEditScreen}
+      options={({ navigation }) => ({
+        // tabBarButton: () => (
+        //   <NewListingButton
+        //     onPress={() => navigation.navigate("ListingEdit")}
+        //   />
+        // ),
+        tabBarIcon: ({ color, size }) => (
+          <MaterialCommunityIcons
+            name="plus-circle"
+            color={color}
+            size={size}
+          />
+        ),
+      })}
+    />
+    <Tab.Screen
+      name="Account"
+      component={AccountNavigator}
+      options={{
+        tabBarIcon: ({ color, size }) => (
+          <MaterialCommunityIcons name="account" color={color} size={size} />
+        ),
+      }}
+    />
   </Tab.Navigator>
 );
 
-export default AuthNavigator;
+export default AppNavigator;
